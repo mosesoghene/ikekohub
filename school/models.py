89e38ee55d8cedbf548044f50ecd61a12ced3d76
Olokor <102165988+Olokor@users.noreply.github.com>
2025-06-23 20:22:26 +0100

    made changes to tenant schema

--- school/models.py
+++ school/models.py
@@ -1,17 +1,36 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
-
-class AdminUserProfile(models.Model):
+class TenantUsers(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='admin_profile')
 
-    class Role(models.TextChoices):
-        SUPER_ADMIN = 'super_admin', 'Super Admin'
-        SCHOOL_ADMIN = 'school_admin', 'School Admin'
+    ADMIN = 'admin'
+    TEACHER = 'teacher'
+    STUDENT = 'student'
 
-    role = models.CharField(max_length=50, choices=Role)
+    ROLE_CHOICES = [
+        (ADMIN, 'Admin'),
+        (TEACHER, 'Teacher'),
+        (STUDENT, 'Student'),
+    ]
+
+    role = models.CharField(max_length=50, choices=ROLE_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    @property
+    def is_admin(self):
+        return self.role == self.ADMIN
+
+    @property
+    def is_teacher(self):
+        return self.role == self.TEACHER
+
+    @property
+    def is_student(self):
+        return self.role == self.STUDENT
     def __str__(self):
         return f"{self.user.username} - {self.role}"
