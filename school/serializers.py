9f2052c9732bbe9c1bf44e5c70498e2606d2839a
Olokor <102165988+Olokor@users.noreply.github.com>
2025-06-26 21:18:26 +0100

    Add admin to tenant for testing, JWT for Authorisation Tenant User permission Tenant Users(Admin, teacher, Parent, Student) First Api endpoint

---
+++ school/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from school.models import TenantUser
+
+class BaseUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TenantUser
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+
+class AdminProfileSerializer(serializers.ModelSerializer):
+    user = BaseUserSerializer(read_only=True)
+    class Meta:
+        model = TenantUser
+        fields = '__all__'
